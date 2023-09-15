0434f0ceaaa602d97100d5f9ceac9f0204e2a61e
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-15 14:22:27 -0300

    feat: :art: App
    *Alterado o arquivo para aceitar typescript;  *Adicionado o logo como component para fins de teste

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ReactComponent as Logo } from '../src/assets/images/svg/logo.svg';
+
+function App() {
+
+  return (
+    <>
+      <div style={{color: 'black', fontSize: '30px', borderRadius: '20px'}}>
+        <Logo />
+      </div>
+    </>
+  )
+}
+
+export default App
