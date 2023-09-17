31a03b6f71631070b0eb6a89f56b7c50eac3321d
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-17 08:29:37 -0300

    feat: :art: Constants
    *Criado pasta para separar as constantes do projeto;

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+import { LabelsData } from "../types/ConstantsData";
+
+export const API_KEY: string = "RVZG0GHEV2KORLNA";
+export const TO_SYMBOL: string = "BRL";
+
+export const LABELS: LabelsData = {
+  INPUT: "Enter the currency code",
+  BUTTON: "Exchange Result",
+  FOOTER: "Copyright 2022 - Action Labs",
+  CURRENT_TITLE: "Exchange Rate now",
+  MAIN_TITLE: "BRL Exchange Rate",
+};
