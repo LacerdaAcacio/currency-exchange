5348e3eed09d45f427d289b30c96084a96ca7b4a
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-17 08:11:41 -0300

    feat: :art: [Components]Footer
    *Componente fixo do footer, seguindo o layout do FIGMA.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { LABELS } from "../../constants";
+import { FooterContainer } from "../../styles/styles";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterContainer>{LABELS.FOOTER}</FooterContainer>
+    </>
+  );
+};
+
+export default Footer;
