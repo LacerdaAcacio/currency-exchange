4a1a55710120aa856b49b7bbd1a79408dda7dcc8
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-17 23:12:58 -0300

    feat: :lipstick: [styles]CurrentExchange
    *Separado/movido styles de styles.ts que só eram utilizados em um componente.

---
+++ src/components/CurrentExchange/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ExchangeContainer = styled.div`
+  width: 20.625rem;
+`;
+
+export const StyledNormalH3 = styled.h3`
+  font-weight: normal;
+  color: #333333;
+`;
+
+export const CurrentExchangeCard = styled.div`
+  margin: 1.25rem 0;
+  display: flex;
+  align-items: center;
+  background: #e6f7ff;
+  height: 4.5rem;
+  font-family: Roboto;
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 2.5rem;
+`;
