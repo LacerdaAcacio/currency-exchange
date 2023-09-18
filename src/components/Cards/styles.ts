fb1e8382470f0175b9fe87f234c40be7b6103482
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-17 23:14:12 -0300

    feat: :art: [styles]Cards
    *Refatorado; *Modificado unidades de medida para aumentar a responsividade; *Criado SharedStyles para não ficar repetindo propriedades dentro de componentes diferentes; *Separado/movido styles de styles.ts que só eram utilizados em um componente.

--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -1,16 +1,33 @@
-import { styled } from "styled-components";
+import styled, { css } from "styled-components";
 
 interface Props {
   isHigher?: boolean;
 }
 
+const flexCenterStyles = css`
+  display: flex;
+  align-items: center;
+`;
+
+const boxShadowStyle = css`
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.1);
+`;
+
 export const DailyCard = styled.div`
+  ${boxShadowStyle}
   background-color: #ffffff;
   width: 100%;
   height: 100%;
-  box-shadow: 0px 2px 8px 0px #0000001a;
-  padding: 16px;
-  margin: 8px;
+  padding: 1rem;
+  margin: 0.5rem;
+`;
+
+export const FlexAlignedDiv = styled.div`
+  ${flexCenterStyles}
+`;
+
+export const RightMarginedSpan = styled.span`
+  margin-right: 0.75rem;
 `;
 
 export const LowerHigherField = styled.p<Props>`
