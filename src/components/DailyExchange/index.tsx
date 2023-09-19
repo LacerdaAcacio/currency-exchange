559d12a910e6b70acd1c9c0c5b5794817e95bbd0
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-19 00:42:02 -0300

    feat: :art: [components]DailyExchange
    *Enviado algumas funções para useExchange; *Adaptado funções que estavam em useExchange e não estavam funcionando bem; *Ajuste nos imports; *ESLint -> Ajuste na ordem das funções;  *Refatoração; *Adicionado uuidv4 function para gerar key dos cards;

--- src/components/DailyExchange/index.tsx
+++ src/components/DailyExchange/index.tsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { AccordionSummary } from "@mui/material";
 
+import { v4 as uuidv4 } from "uuid";
 import { useExchange } from "../../hooks/useExchange";
 import { useFetchExchange } from "../../hooks/useFetchExchange";
+
 import { LABELS } from "../../constants";
-import { Formatter } from "../../utils/Formatter";
 
 import { DailyExchangeParams } from "../../types/ParamsData";
 import { DailyExchangeData } from "../../types/DailyExchangeData";
 
+import Cards from "../Cards";
+import { FlexContainer } from "../../styles/styles";
 import {
   StyledAccordion,
   StyledAccordionDetails,
@@ -17,48 +20,31 @@ import {
   StyledRemoveIcon,
 } from "./styles";
 
-import Cards from "../Cards";
-import { FlexContainer } from "../../styles/styles";
-
-const DailyExchange = ({ formData }: DailyExchangeParams) => {
-  const [expanded, setExpanded] = useState(false);
+function DailyExchange({ formData }: DailyExchangeParams) {
+  const [isExpanded, setIsExpanded] = useState(false);
   useForm();
-  const { getLastThirtyDays, generateKey } = useExchange();
+  const { filterLastThirtyDays, compareCloseToPrevious } = useExchange();
   const { onSubmit, exchangeData, hasExchangeData } = useFetchExchange();
-
   const dailyTitle = LABELS.DAILY_TITLE.toUpperCase();
 
   const handleExpand = () => {
-    setExpanded(!expanded);
-    if (!expanded) {
+    setIsExpanded(!isExpanded);
+    if (!isExpanded) {
       onSubmit(formData, true);
     }
   };
 
   const lastThirtyDays =
-    hasExchangeData && expanded ? getLastThirtyDays(exchangeData?.data) : [];
+    hasExchangeData && isExpanded
+      ? filterLastThirtyDays(exchangeData?.data)
+      : [];
 
-  const compareCloseToPrevious = (data: DailyExchangeData, index: number) => {
-    const currentClose = Number(data.close);
-    const previousClose = Number(
-      lastThirtyDays[index + 1]?.close || currentClose,
-    );
-
-    const comparisonPercentage = Formatter.ruleOfThreePercent(
-      currentClose,
-      previousClose,
-    );
-
-    const result = Number(comparisonPercentage) - 100;
-    return result.toFixed(2);
-  };
-
-  const mapFunction = (data: DailyExchangeData, index: number) => {
-    const comparison = compareCloseToPrevious(data, index);
+  const renderCards = (data: DailyExchangeData, index: number) => {
+    const comparison = compareCloseToPrevious(data, index, lastThirtyDays);
     return (
-      <FlexContainer key={generateKey}>
+      <FlexContainer key={uuidv4()}>
         <Cards
-          key={generateKey}
+          key={uuidv4()}
           dailyExchangeData={data}
           comparison={comparison}
         />
@@ -67,21 +53,19 @@ const DailyExchange = ({ formData }: DailyExchangeParams) => {
   };
 
   return (
-    <>
-      <StyledAccordion expanded={expanded} onChange={handleExpand}>
-        <AccordionSummary
-          expandIcon={expanded ? <StyledRemoveIcon /> : <StyledAddIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <p>{dailyTitle}</p>
-        </AccordionSummary>
-        <StyledAccordionDetails>
-          {hasExchangeData && lastThirtyDays?.map(mapFunction)}
-        </StyledAccordionDetails>
-      </StyledAccordion>
-    </>
+    <StyledAccordion expanded={isExpanded} onChange={handleExpand}>
+      <AccordionSummary
+        expandIcon={isExpanded ? <StyledRemoveIcon /> : <StyledAddIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <p>{dailyTitle}</p>
+      </AccordionSummary>
+      <StyledAccordionDetails>
+        {hasExchangeData && lastThirtyDays.map(renderCards)}
+      </StyledAccordionDetails>
+    </StyledAccordion>
   );
-};
+}
 
 export default DailyExchange;
