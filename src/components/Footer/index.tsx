2a1c39cfa57b47b68e194fd5e3e27520aa0a6f09
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-19 00:41:21 -0300

    refactor: :recycle: [components]Footer
    *Refatoração; *Ajustes ESLint;

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,8 @@
 import { LABELS } from "../../constants";
 import { FooterContainer } from "../../styles/styles";
 
-const Footer = () => {
-  return (
-    <>
-      <FooterContainer>{LABELS.FOOTER}</FooterContainer>
-    </>
-  );
-};
+function Footer() {
+  return <FooterContainer>{LABELS.FOOTER}</FooterContainer>;
+}
 
 export default Footer;
