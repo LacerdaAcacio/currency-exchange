613db35da03575d400c1516e2954beb42479d9df
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-19 00:49:20 -0300

    ci: :wrench: [configs]eslintignore
    *Criado e adicionado vite.config.js (por algum motivo ele não estava sendo adicionado no tsconfig).

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'prettier'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module", "project": "./tsconfig.json" },
+  settings: { react: { version: "18.2" }, "import/extensions": [".js", ".jsx", ".ts", ".tsx"], },
+  plugins: ["react-refresh", "prettier", "import" ],
   rules: {
-    'prettier/prettier': 'error',
-    'react-refresh/only-export-components': [
-      'warn',
+    "prettier/prettier": "error",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
 }
