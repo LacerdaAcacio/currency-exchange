9ccbc6c16fbd14ea31014ef87417ef9697427220
LacerdaAcacio <acacio.lacerda@hotmail.com>
2023-09-19 00:46:56 -0300

    refactor: :recycle: [src]main.tsx
    *Adaptado todo o main.jsx para typescript e corrigido todos os erros ESLint e de import;

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
-import GlobalStyle from "./styles/global.ts";
-import App from "./App.tsx";
+import GlobalStyle from "./styles/global.js";
+import App from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("No root element found");
+}
+
+const root = ReactDOM.createRoot(rootElement);
 const queryClient = new QueryClient();
 
 root.render(
